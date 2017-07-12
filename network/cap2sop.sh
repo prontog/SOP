158d8f37af9ce9f08d1855fff7a1da71d2a6796d
Panos Rontogiannis <p_ronto@hotmail.com>
2017-07-12 17:33:50 +0300

    Updated -v option to also enable SOP protocol tracing.

--- network/cap2sop.sh
+++ network/cap2sop.sh
@@ -3,7 +3,7 @@
 # Function to output usage information
 usage() {
   cat <<EOF
-Usage: ${0##*/} [OPTION] CAP_FILE... 
+Usage: ${0##*/} [OPTION] CAP_FILE...
 Extract SOP message information from CAP_FILEs.
 
 Options:
@@ -15,11 +15,12 @@ EOF
 
 # By default redirect tshark's STDERR to /dev/null.
 TSHARK_STDERR='2>/dev/null'
-
+SOP_TRACE=
 while getopts "hv" option
 do
 	case $option in
-		v) TSHARK_STDERR=;;
+		v) TSHARK_STDERR=
+		   SOP_TRACE="-o sop.trace:TRUE";;
 		\?) usage;;
 	esac
 done
@@ -32,7 +33,7 @@ fi
 
 TSHARK_DISP_FILTER="-Y sop"
 TSHARK_OUT_FIELDS="-e frame.number -e _ws.col.Time -e sop.msgtype -e sop.clientid -e eth.src -e eth.dst -e ip.src -e ip.dst"
-TSHARK_CMD="tshark $TSHARK_DISP_FILTER -T fields -E header=n -E separator=',' -E aggregator=';' -o column.format:'Time,%At' $TSHARK_OUT_FIELDS"
+TSHARK_CMD="tshark $SOP_TRACE $TSHARK_DISP_FILTER -T fields -E header=n -E separator=',' -E aggregator=';' -o column.format:'Time,%At' $TSHARK_OUT_FIELDS"
 
 # Print header.
 echo frame,dateTime,msgType,clientId,ethSrc,ethDst,ipSrc,ipDst,capFile
@@ -48,7 +49,7 @@ do
 	set -o errexit
 
 	eval $TSHARK_CMD -r $CAP_FILE $TSHARK_STDERR | awk -v CAP_FILE="${CAP_FILE##*/}" '
-	BEGIN { 
+	BEGIN {
 		FS = ","
 		OFS = ","
 		# These are the msg types that contain the clientId field. All other
@@ -66,34 +67,34 @@ do
 		ethDst = $6
 		ipSrc = $7
 		ipDst = $8
-		
-		# Copy the messages types that are included in msgTypesToPrint to 
+
+		# Copy the messages types that are included in msgTypesToPrint to
 		# array filteredMsgTypes.
 		fi = 0
 		for(i in msgTypes) {
 			if (match(msgTypesToPrint, msgTypes[i])) {
 				fi++
-				filteredMsgTypes[fi] = msgTypes[i]			
+				filteredMsgTypes[fi] = msgTypes[i]
 			}
 		}
-		
+
 		# Skip line if there was no messages to print.
 		if (fi == 0) {
 			next
 		}
-		
+
 		# filteredMsgTypes should have the same length with clientIds.
 		if (length(filteredMsgTypes) != length(clientIds)) {
-			printf("Skipping frame %d because of missing fields (%d, %d).", 
+			printf("Skipping frame %d because of missing fields (%d, %d).",
 					frame, length(filteredMsgTypes), length(clientIds)) > "/dev/stderr"
 			next
 		}
-		
+
 		for(i in filteredMsgTypes) {
-			print frame, dateTime, filteredMsgTypes[i], clientIds[i], 
+			print frame, dateTime, filteredMsgTypes[i], clientIds[i],
 				  ethSrc, ethDst, ipSrc, ipDst, CAP_FILE
 		}
-		
+
 		# Clean up array filteredMsgTypes before moving to the next line.
 		delete filteredMsgTypes
 	}'
