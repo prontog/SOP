c582145a32a3a8161af77f12582d80f3efa84b2d
Panos Rontogiannis <p_ronto@hotmail.com>
2017-07-12 18:14:39 +0300

    Fixed env vars so that they can be overridden. Mainly to support Windows...

--- env.sh
+++ env.sh
@@ -2,8 +2,8 @@
 # Prepares SOP environment. This file is meant to be sourced.
 
 # Add necessary env vars.
-export SOP=${BASH_SOURCE%/*}
-export WSDH_SCRIPT_PATH=~/ws_dissector_helper/src/
-export SOP_SPECS_PATH=$SOP/specs/
+export SOP=${SOP-${BASH_SOURCE%/*}}
+export WSDH_SCRIPT_PATH=${WSDH_SCRIPT_PATH-~/ws_dissector_helper/src/}
+export SOP_SPECS_PATH=${SOP_SPECS_PATH-$SOP/specs/}
 # Update PATH with all dirs containing scripts.
 PATH=${PATH}:$SOP/network
--- network/init.lua
+++ network/init.lua
@@ -1,4 +1,8 @@
-WSDH_SCRIPT_PATH = os.getenv('WSDH_SCRIPT_PATH') or persconffile_path("")
-SOP_SPECS_PATH = os.getenv('SOP_SPECS_PATH') or persconffile_path("") .. "sop"
-SOP = os.getenv('SOP') or "/vagrant/sop"
-dofile(SOP .. '/network/sop.lua')
+WSDH_SCRIPT_PATH = os.getenv("WSDH_SCRIPT_PATH") or persconffile_path("")
+SOP_SPECS_PATH = os.getenv("SOP_SPECS_PATH") or persconffile_path("") .. "sop"
+if os.getenv('SOP') then
+	SOP = os.getenv('SOP') .. '/network'
+else
+	SOP = persconffile_path('') .. 'sop'
+end
+dofile(SOP .. '/sop.lua')
