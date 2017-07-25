20ada19132ca28576e01348afff495b2f9dbdfe3
Panos Rontogiannis <p_ronto@hotmail.com>
2017-07-25 17:48:36 +0300

    Added CSV file with all SOP protocol versions. Also added helper script that updated SOP_SPEC_PATH according to the date in a filename passed as argument.

---
+++ specs/sop_specs_path.sh
@@ -0,0 +1,67 @@
+#!/usr/bin/env bash
+
+# Function to output usage information
+usage() {
+  cat <<EOF
+Usage: source ${0##*/} [OPTION] FILE
+Update SOP_SPEC_PATH environment variable to the directory best-matching the
+filename.
+
+It looks for a DATE in the filename with either the format YYYY-MM-DD
+or YYYY_MM_DD. Then it looks in $SOP/specs/versions.csv for the latest version
+at that DATE. Finally it updates SOP_SPEC_PATH with the directory with the specs
+of that version of SOP.
+
+If no file is given or the filename has no DATE in it, then SOP_SPEC_PATH is left
+unchanged.
+
+$SOP/specs/versions.txt is a TAB separated file with columns for VERSION,
+RELEASE_DATE and INFO. If the directory $SOP/specs/VERSION is missing then
+SOP_SPEC_PATH is set to $SOP/specs/.
+
+NOTE that this script should only be sourced!
+
+Options:
+  -h	display this help text and exit
+EOF
+  exit 1
+}
+
+while getopts "h" option
+do
+	case $option in
+		h) usage;;
+        *) exit 1;;
+	esac
+done
+
+shift $(( $OPTIND - 1 ))
+
+if [[ $# -eq 0 ]]; then
+    echo "No file given, SOP_SPECS_PATH is left unchanged." >&2
+	return
+fi
+
+FILE=$1
+DATE=$(echo $FILE | sed -nr 's/.*([[:digit:]]{4}[_-][[:digit:]]{2}[_-][[:digit:]]{2}).*/\1/p')
+
+if [[ -z $DATE ]]; then
+    echo "No date found in $FILE, SOP_SPECS_PATH is left unchanged." >&2
+	return
+fi
+
+# Find the latest version at DATE. It concats vrsion.csv and a dummy line with
+# the DATE and sort the lines according to the release dates. Finally pick the line
+# above the DATE.
+version_line=$({
+    cat $SOP/specs/versions.csv;
+    printf "_\t%s\t_" $DATE;
+} | sort -k 2,2 | grep -B1 "^_[[:space:]]$DATE[[:space:]]_$" | head -1)
+
+read spec_dir release_date info < <(echo $version_line)
+if [[ -d "$SOP/specs/$spec_dir" ]]; then
+    SOP_SPECS_PATH="$SOP/specs/$spec_dir/"
+    echo "Updated SOP_SPECS_PATH to $SOP_SPECS_PATH" >&2
+else
+    SOP_SPECS_PATH="$SOP/specs/"
+fi
